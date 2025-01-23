aebd420ef412e7d7e58d44c5bd131e4d6186d0c4
Henry Lee <henryleeworld@gmail.com>
2025-01-24 00:08:39 +0800

    feat:更新最新資料

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
             'name' => 'test user',
             'email' => 'test@test.com',
             'email_verified_at' => now(),
-            'password' => Hash::make('password'),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'role_id' => User::ROLE_ADMIN,
         ]);
 
